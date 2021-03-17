4d25b42af2c6a11375d2fda497aecae685e593e4
Nikol <39650438+nikolst@users.noreply.github.com>
2021-03-17 17:08:49 +0200

    Update StringInput.cpp

--- Strings/StringInput.cpp
+++ Strings/StringInput.cpp
@@ -13,5 +13,5 @@ int main()
     std::cout << "Enter your string: " << std::endl;
     std::getline(std::cin, str2); //if the string is "Hello World!"
     std::cout << str2 << std::endl; //the output is going to be "Hello World!"
-    
+    return 0;
 }

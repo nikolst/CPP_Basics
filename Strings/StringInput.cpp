ae70155a1ffa124d9b8f1237c9c13a5242e75f3c
Nikol <39650438+nikolst@users.noreply.github.com>
2021-03-17 17:06:08 +0200

    C++ Primer (ex. 3.2)
    Write a program to read the standard input a line at a time. Modify your program to read a word at a time.

---
+++ Strings/StringInput.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    /*
+    std::string str1;
+    std::cout << "Enter your string: " << std::endl;
+    std::cin >> str1; //if the string is "Hello World!"
+    std::cout << str1 << std::endl; //the output is going to be "Hello"
+    */
+  
+    std::string str2;
+    std::cout << "Enter your string: " << std::endl;
+    std::getline(std::cin, str2); //if the string is "Hello World!"
+    std::cout << str2 << std::endl; //the output is going to be "Hello World!"
+    
+}
